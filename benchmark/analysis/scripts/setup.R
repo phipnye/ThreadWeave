1c4793b9829a9e4583ffe8fbff0d27386919f556
phipnye <phipnye@proton.me>
2026-08-06 08:20:40 -0700

    Finish setting up R plot workflow

--- benchmark/analysis/scripts/setup.R
+++ benchmark/analysis/scripts/setup.R
@@ -1,21 +1,26 @@
+ANALYSIS <- function(...) {
+  script_dir <- if (!is.null(sys.frames()[[1]]$ofile)) {
+    dirname(sys.frames()[[1]]$ofile)
+  } else {
+    getwd()
+  }
+  
+  normalizePath(file.path(script_dir, "..", ...), mustWork = FALSE)
+}
+
 if (!requireNamespace("renv", quietly = TRUE)) {
   install.packages("renv", repos = "https://cloud.r-project.org/")
 }
 
-renv::restore(prompt = FALSE)
+renv::restore(lockfile = ANALYSIS("renv.lock"), prompt = FALSE)
 
 suppressPackageStartupMessages({
-  library(data.table)
-  library(ggplot2)
-  library(jsonlite)
-  library(scales)
-  library(stringr)  
+library(jsonlite)
+library(data.table)
+library(ggplot2)
+library(stringr)
+library(scales)
 })
 
-JSONS <- function(x = "") {
-  file.path("..", "jsons", x)
-}
-
-PLOTS <- function(x = "") {
-  file.path("..", "plots", x)
-}
+JSONS <- function(file) ANALYSIS("jsons", file)
+PLOTS <- function(file) ANALYSIS("plots", file)
